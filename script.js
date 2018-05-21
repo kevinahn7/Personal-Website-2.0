7d8928b950a5782a6f8363e3dbcea9075410c997
Guest User <Guest@Epicodus-8C.local>
2018-05-21 14:55:35 -0700

    Fix JavaScript/jQuery issue

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-$("img").on("click", function() {
-    alert("No real projects yet...");
-});   //why is this not working?
+$(document).ready(function() {
+  $("img").on("click", function() {
+      alert("No real projects yet...");
+  });   //why is this not working?
+});
