1a10c71192ac1eff8fe45c8ef4a82147154f8d3e
Kevin Ahn <kevinahn7@hotmail.com>
2018-05-21 15:39:38 -0700

    Add nav scroll function

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
   $("img").on("click", function() {
-      alert("No real projects yet...");
-  });   //why is this not working?
+    alert("No real projects yet...");
+  });
+
+  $('#contact-scroll-to').click(function() {
+    $('html, body').animate({
+      scrollTop: $('#contact').offset().top
+    }, 500);
+    return false;
+  });
 });
